f5ad5a5de56f96dab61322b1670d792cb82c1267
Jérôme FOURMOND <jerome@fourmond.fr>
2016-09-04 19:22:07 +0200

    Commit 27
    * Longueur du mot de passe (25 à 100)

--- EzChat/bd/script.sql
+++ EzChat/bd/script.sql
@@ -1,24 +1,30 @@
 /*	DELETE USER si existe	*/
-DROP USER IF EXISTS 'root'@'localhost';
+/* DROP USER IF EXISTS 'root'@'localhost'; */
+
+/* DROP TABLE si existe */
+DROP TABLE IF EXISTS users;
 
 /* DROP DATABASE si existe */
 DROP DATABASE IF EXISTS ezchat_server;
 CREATE DATABASE ezchat_server DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE ezchat_server;
 
-/* DROP TABLE si existe */
-DROP TABLE IF EXISTS users;
-
 /* Table des utilisateur */
 CREATE TABLE users (
 	id INT(10) NOT NULL AUTO_INCREMENT,
 	username VARCHAR(25) NOT NULL UNIQUE,
-	password VARCHAR(25) NOT NULL,
+	password VARCHAR(100) NOT NULL,
+    inscription_date DATETIME NOT NULL DEFAULT NOW(),
+    last_connexion DATETIME DEFAULT NULL,
+    last_message DATETIME DEFAULT NULL,
+    count_message INT(10) NOT NULL DEFAULT 0, 
 	PRIMARY KEY (id)
 );
 ALTER TABLE users CONVERT TO CHARACTER SET 'UTF8';
 
 /*	CREATE USER FOR DATABASE	*/
+/*
 CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON ezchat_server.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
+*/
--- EzChat/bd/test_add.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (username, password) VALUES (
-	"Jerome",
-    "123"
-);
